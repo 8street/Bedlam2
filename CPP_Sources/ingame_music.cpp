42ff218074d75d6b32d318b0dd53b7810cfee522
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-07-25 15:59:01 +0300

    Revert delete files

---
+++ CPP_Sources/ingame_music.cpp
@@ -0,0 +1,11 @@
+#include "ingame_music.h"
+
+void play_wav_music(int track)
+{
+
+}
+
+void stop_wav_music()
+{
+
+}
---
+++ CPP_Sources/ingame_music.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern "C" void play_wav_music(int track);
+extern "C" void stop_wav_music();
