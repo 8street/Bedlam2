84d159944d009cc65adbc4f4bf7a6b6dd8e5718e
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-08-22 17:25:17 +0300

    Edit resolutions settings

--- CPP_Sources/options.cpp
+++ CPP_Sources/options.cpp
@@ -5,16 +5,17 @@
 
 Options::Options()
 { 
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(640,  480),    2, 36, 36,  9,  9, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(800,  600),    1, 46, 36, 12, 12, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1024, 768),    1, 56, 46, 15, 15, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1280, 1024),  -3, 70, 50, 15, 15, 304, -384));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1600, 1200),   2, 36, 36,  9,  9, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1080), -13, 90, 70, 15, 15, 304, -720));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1200),   2, 36, 36,  9,  9, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(2048, 1080),   2, 36, 36,  9,  9, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(3840, 2160),   2, 36, 36,  9,  9, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(7680, 4320),   2, 36, 36,  9,  9, 304, -256));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(640,  480),    2, 36,  36,  9,  9, 304, -256));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(800,  600),    1, 46,  36, 12, 12, 304, -256));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1024, 768),    1, 56,  46, 15, 15, 304, -256));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1280, 1024),  -3, 70,  50, 15, 15, 304, -384));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1600, 1200),  -7, 80,  65, 15, 15, 304, -512));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1080), -13, 90,  70, 15, 15, 304, -720));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1200), -13, 100, 70, 15, 15, 304, -720));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(2048, 1080), -15, 100, 70, 15, 15, 304, -720));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(2560, 1440), -21, 120, 90, 15, 15, 304, -960));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(3840, 2160), -13, 100, 70, 15, 15, 304, -768));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(7680, 4320),   2, 36,  36,  9,  9, 304, -256));
 
     std::sort(m_resolutions_settings.begin(), m_resolutions_settings.end());
 }
