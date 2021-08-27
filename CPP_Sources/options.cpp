ef2c6d7e6ee22061b40c34f692425925add64496
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-08-27 18:28:50 +0300

    Edit resolution settings

--- CPP_Sources/options.cpp
+++ CPP_Sources/options.cpp
@@ -23,12 +23,12 @@ Options::Options()
     m_resolutions_settings.push_back(Resolution_settings(Resolution(640,  480),    2, 36,  36,  9,  9, 304, -256));
     m_resolutions_settings.push_back(Resolution_settings(Resolution(800,  600),    1, 46,  36, 12, 12, 304, -256));
     m_resolutions_settings.push_back(Resolution_settings(Resolution(1024, 768),    1, 56,  46, 15, 15, 304, -256));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1280, 1024),  -3, 70,  50, 15, 15, 304, -384));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1600, 1200),  -7, 80,  65, 15, 15, 304, -512));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1080), -13, 90,  70, 15, 15, 304, -736));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1200), -13, 100, 70, 15, 15, 304, -736));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(2048, 1080), -15, 100, 70, 15, 15, 304, -736));
-    m_resolutions_settings.push_back(Resolution_settings(Resolution(2560, 1440), -21, 120, 90, 15, 15, 304, -960));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1280, 1024),  -3, 70,  60, 16, 18, 304, -384));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1600, 1200),  -7, 80,  65, 16, 26, 304, -512));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1080), -14, 90,  70, 16, 32, 304, -736));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(1920, 1200), -14, 100, 70, 16, 32, 304, -736));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(2048, 1080), -15, 100, 70, 16, 36, 304, -768));
+    m_resolutions_settings.push_back(Resolution_settings(Resolution(2560, 1440), -24, 120, 90, 16, 36, 304, -1056));
 
     std::sort(m_resolutions_settings.begin(), m_resolutions_settings.end());
 }
