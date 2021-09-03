e860ffd76b870e36bfc48c17ff7a5d68d873638e
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-09-03 21:43:51 +0300

    Test yml1

--- linux/prepare.sh
+++ linux/prepare.sh
@@ -12,22 +12,22 @@ sudo cp jwasm /usr/local/bin
 cd ..
 
 
-#git clone https://github.com/libsdl-org/SDL
-#cd SDL
-#mkdir build
-#cd build
-#../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
-#make
-#sudo make install
-#cd ..
-#cd ..
+git clone https://github.com/libsdl-org/SDL
+cd SDL
+mkdir build
+cd build
+../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
+make
+sudo make install
+cd ..
+cd ..
 
-#git clone https://github.com/libsdl-org/SDL_mixer
-#cd SDL_mixer
-#mkdir build
-#cd build
-#../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
-#make
-#sudo make install
-#cd ..
-#cd ..
+git clone https://github.com/libsdl-org/SDL_mixer
+cd SDL_mixer
+mkdir build
+cd build
+../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
+make
+sudo make install
+cd ..
+cd ..
