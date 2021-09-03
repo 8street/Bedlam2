2d376fd59a0abcd3a1dac7da05c901e7dc9b81f9
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2021-09-03 21:41:45 +0300

    Test yml1

--- linux/prepare.sh
+++ linux/prepare.sh
@@ -6,24 +6,28 @@ sudo apt install clang-12 -y
 
 git clone https://github.com/JWasm/JWasm.git
 cd JWasm 
-cmake . 
+cmake .
 make
-sudo cp jwasm /usr/local/bin 
+sudo cp jwasm /usr/local/bin
+cd ..
 
 
-git clone https://github.com/libsdl-org/SDL
-cd SDL
-mkdir build
-cd build
-../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
-make
-sudo make install
+#git clone https://github.com/libsdl-org/SDL
+#cd SDL
+#mkdir build
+#cd build
+#../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
+#make
+#sudo make install
+#cd ..
+#cd ..
 
-
-git clone https://github.com/libsdl-org/SDL_mixer
-cd SDL_mixer
-mkdir build
-cd build
-../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
-make
-sudo make install
+#git clone https://github.com/libsdl-org/SDL_mixer
+#cd SDL_mixer
+#mkdir build
+#cd build
+#../configure --host=i686-pc-linux-gnu CFLAGS=-m32 CXXFLAGS=-m32 LDFLAGS=-m32
+#make
+#sudo make install
+#cd ..
+#cd ..
