57e9a378012abc0c469049678a4bb8586330cc20
Nikolai Ekaterinin <nikolai_e@rambler.ru>
2021-12-02 09:51:36 +0300

    Refactor SDL_events

--- CPP_Sources/sdl_event.h
+++ CPP_Sources/sdl_event.h
@@ -1,3 +1,10 @@
 #pragma once
 
 extern "C" int SDL_events();
+
+int quit_events(const SDL_Event &event);
+int mouse_events(const SDL_Event &event);
+int palette_events(const SDL_Event &event);
+int cursor_events(const SDL_Event &event);
+int keyboard_events(const SDL_Event &event);
+int window_events(const SDL_Event &event);
